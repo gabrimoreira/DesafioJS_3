21be2f91cddf298984391c341ef9416f0453a76b
gabrimoreira <110856732+gabrimoreira@users.noreply.github.com>
2023-11-18 21:31:58 -0300

    Criação CriarPokemonCard: adicionei a função que cria o card pokemon

--- script/script.js
+++ script/script.js
@@ -1,3 +1,6 @@
+const pokemons = [];
+const pokemonsClicados = [];
+
 async function fetchPokemonData() {
     for (let i = 1; i <= 151; i++) {
       //recebe o dado e não a promessa
@@ -15,5 +18,30 @@ async function fetchPokemonData() {
       card.addEventListener('click', () => cardClick(card, pokemon));
       cardsContainer.appendChild(card);
     });
+  }
+  
+  function criaPokemonCard(pokemon) {
+    const card = document.createElement('div');
+    card.classList.add('card');
+  
+    card.innerHTML = `
+      <img src="${pokemon.sprites.front_default}" class="card-img-pokemon" alt="${pokemon.name}">
+      <div class="card-body">
+        <h5 class="card-title">${pokemon.name}</h5>
+        <p class="card-text">ID: ${pokemon.id}</p>
+        <p class="card-text">Type: ${pokemon.types[0].type.name}</p>
+      </div>
+      <div class="card-back">
+      <p>Status:</p>
+      <ul>
+        <li>HP: ${pokemon.stats[0].base_stat}</li>
+        <li>Attack: ${pokemon.stats[1].base_stat}</li>
+        <li>Defense: ${pokemon.stats[2].base_stat}</li>
+        <!-- Adicione mais status conforme necessário -->
+      </ul>
+    </div>
+    `;
+  
+    return card;
   }
   
